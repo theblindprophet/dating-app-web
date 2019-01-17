2139d0d9edcea8dc6fb32bb1168129a4c8dfd5b5
Gross Jamie <jamie.gross@partner.bmwgroup.com>
2019-01-17 09:23:33 -0500

    End of section 5

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -17,7 +17,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in');
     }, err => {
-      console.log('Error');
+      console.log(err);
     });
   }
 
